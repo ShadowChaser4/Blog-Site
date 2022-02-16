f70b715cbfa11f7bdccd9570acdaf7b247b6e8ca
ShadowChaser48v <kushalacharya48v@outlook.com>
2022-02-16 11:46:46 +0545

    after the challange 19

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express() ;
 const bodyparser = require('body-parser');
+const lodash = require('lodash');
 
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({extended:true}));
@@ -13,24 +14,40 @@ app.listen(3000, ()=>{
   console.log('The server is listening in the port 3000')
 });
 
-const content = [
+const start= [
   {
   title:'First',
   body: " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gal"
   },
-  {
+ {
     title: "Second",
     body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   }
 ];
 
+const content = [];
+
 
 // for index page
 app.get('/', (req, res)=>{
-  res.render('index',{contents: content} )
-
+  res.render('index',{contents: content, start: start} )
 })
 
+// for other pages
+app.get('/post/:postname', (req, res)=>{
+
+  let postContent ={};
+
+  for (x in content)//checking the titles form the entered titles to titles with us
+  {
+    if(lodash.lowerCase(content[x].title) == lodash.lowerCase(req.params.postname))
+    {
+       postContent = content[x];
+     }
+  }
+  
+    res.render('post',{contents: postContent} )
+})
 
 // for contact page
 app.get('/contact', (req, res)=>{
@@ -54,9 +71,6 @@ app.post('/compose', (req, res)=>{
     body: (req.body.ComposedString)
   }
   content.push(post);
-  content.forEach((cont) => {
-    console.log(cont.title)
-  });
 
-  res.redirect('/')
+  res.redirect('/compose')
 })
